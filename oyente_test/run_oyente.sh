0240a6deec9ce09538624d1206f729a5721aa01b
haoyu wang <1363585348@qq.com>
2023-04-21 17:26:58 +0800

    new sh

---
+++ oyente_test/run_oyente.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
+do
+	filename=$(basename"$file")
+	python -u oyente.py -s "file" > "oyente_testResults/oyente_${filename%.*}.txt"
+done
