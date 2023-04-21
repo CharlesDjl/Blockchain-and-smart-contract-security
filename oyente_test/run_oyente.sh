516acf20c3fd960443e33e745b452e6b1847f2b7
WHY <1363585348@qq.com>
2023-04-21 10:08:33 +0000

    final results

--- oyente_test/run_oyente.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
-do
-	filename=$(basename "$file")
-	python -u oyente.py -s "$file" 2>&1 | tee "./oyente_testResults/oyente_${filename%.*}.txt" > /dev/null
-done
