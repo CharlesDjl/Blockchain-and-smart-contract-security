eee18de0a8c6fe0dea75356ca5e7499f2c45cf42
haoyu wang <1363585348@qq.com>
2023-04-21 17:37:19 +0800

    final sh

--- oyente_test/run_oyente.sh
+++ oyente_test/run_oyente.sh
@@ -2,6 +2,6 @@
 
 for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
 do
-	filename=$(basename"$file")
+	filename=$(basename "$file")
 	python -u oyente.py -s "$file" > "./oyente_testResults/oyente_${filename%.*}.txt"
 done
