b6d0b0469ddd7b11e4ec12a2888da35ffb794c08
WHY <1363585348@qq.com>
2023-04-21 09:18:30 +0000

    test results of oyente

--- oyente_test/run_oyente.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
-do
-	filename=$(basename "$file")
-	python oyente.py -s "$file" > "./oyente_testResults/oyente_${filename%.*}.txt"
-done
