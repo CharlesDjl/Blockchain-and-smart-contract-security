df68c691d3348bd9c94d5f788b3d9ae3b8101468
haoyu wang <1363585348@qq.com>
2023-04-22 16:15:45 +0800

    final osiris

--- osiris_test/run_osiris.sh
+++ osiris_test/run_osiris.sh
@@ -3,5 +3,5 @@
 for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
 do
 	filename=$(basename "$file")
-	python -u oyente.py -s "$file" 2>&1 | tee "./osiris_testResults/osiris_${filename%.*}.txt" > /dev/null
+	python -u osiris/osiris.py -s "$file" 2>&1 | tee "./osiris_testResults/osiris_${filename%.*}.txt" > /dev/null
 done
