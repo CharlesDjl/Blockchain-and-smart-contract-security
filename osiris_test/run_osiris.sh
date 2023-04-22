94cdc138ccc96c63ee268a205994d33157f0c914
WHY <1363585348@qq.com>
2023-04-22 08:24:54 +0000

    final osiris

--- osiris_test/run_osiris.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
-do
-	filename=$(basename "$file")
-	python -u osiris/osiris.py -s "$file" 2>&1 | tee "./osiris_testResults/osiris_${filename%.*}.txt" > /dev/null
-done
