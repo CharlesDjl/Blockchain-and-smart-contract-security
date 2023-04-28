9a198b64e8a0758527e3755588f39801c6700a99
CharlesDeng <870055485@qq.com>
2023-04-28 09:35:33 +0000

    Updated time

--- slither/slither_analyze_all.sh
+++ slither/slither_analyze_all.sh
@@ -11,6 +11,9 @@ function extract_version() {
 RESULTS_DIR="./results"
 mkdir -p "$RESULTS_DIR"
 
+# 初始化全局开始时间
+GLOBAL_START_TIME=$(date +%s)
+
 # 遍历sol_file文件夹中的所有.sol文件
 for SOL_FILE in ../sol_file/*.sol; do
     echo "Processing $SOL_FILE..."
@@ -28,6 +31,17 @@ for SOL_FILE in ../sol_file/*.sol; do
     solc-select use "$PRAGMA_VERSION"
 
     # 运行Slither分析并将输出重定向到结果文件
+    START_TIME=$(date +%s)
     slither "$SOL_FILE" > "$RESULTS_DIR/$(basename "$SOL_FILE" .sol)_results.txt" 2>&1
+    END_TIME=$(date +%s)
+    ELAPSED_TIME=$((END_TIME - START_TIME))
+
+    # 将运行时间记录在结果文件末尾
+    echo "Elapsed time: $ELAPSED_TIME seconds." >> "$RESULTS_DIR/$(basename "$SOL_FILE" .sol)_results.txt"
 done
+
+# 记录全局运行时间
+GLOBAL_END_TIME=$(date +%s)
+GLOBAL_ELAPSED_TIME=$((GLOBAL_END_TIME - GLOBAL_START_TIME))
+echo "Total elapsed time: $GLOBAL_ELAPSED_TIME seconds." >> "$RESULTS_DIR/A_total_time.txt"
 
