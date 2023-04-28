e9b9503a06320f6568aa7a9549d651781be25148
L09an <loganzjy@gmail.com>
2023-04-28 09:12:19 +0000

    update Securify results

---
+++ securify/securify_run_v2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+input_directory="/home/logan/comp5566/Blockchain-and-smart-contract-security/sol_file"
+output_directory="/home/logan/comp5566/Blockchain-and-smart-contract-security/securify/results_v2"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_directory"
+
+# Iterate over the .sol files and run securify in a Docker container
+for input_file_path in "$input_directory"/*.sol; do
+  sol_file="$(basename "$input_file_path")"
+  output_file_path="$output_directory/${sol_file%.sol}.txt"
+
+  # Run securify and measure the execution time
+  start_time=$(date +%s.%N)
+  output=$(sudo docker run -i -v "$input_directory:/share" securify "/share/$sol_file" 2>&1)
+  exit_status=$?
+  end_time=$(date +%s.%N)
+
+  running_time=$(echo "$end_time - $start_time" | bc)
+
+  # Write the results and running time to the output file
+  echo "Running time: $running_time seconds" > "$output_file_path"
+  echo -e "\n$output" >> "$output_file_path"
+
+  # If there was an error, print a message
+  if [ $exit_status -ne 0 ]; then
+    echo "An error occurred while processing $input_file_path. Check the output file for details."
+  fi
+done
