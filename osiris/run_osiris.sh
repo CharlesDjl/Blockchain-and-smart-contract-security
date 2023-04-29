3d97a69bf14d3f0c9f848bbadc8289b64f578d04
haoyu wang <1363585348@qq.com>
2023-04-29 10:39:49 +0800

    shell-3

--- osiris/run_osiris.sh
+++ osiris/run_osiris.sh
@@ -7,13 +7,14 @@ for file in ./Blockchain-and-smart-contract-security/sol_file/*.sol
 do
 	file_count=$((file_count + 1))
 	filename=$(basename "$file")
-	start_time=$(date +%s%3N)
+	start_time=$(date +%s.%3N)
 	python -u osiris/osiris.py -s "$file" 2>&1 | tee "./results/osiris_${filename%.*}.txt" > /dev/null
-	end_time=$((date +%s%3N))
-	elapsed_time=$(($end_time - $start_time))
-        total_time=$(($total_time + $elapsed_time))
+	end_time=$(date +%s.%3N)
+	elapsed_time=$(awk "BEGIN {print ($end_time - $start_time)}")
+        total_time=$(awk "BEGIN {print $total_time + $elapsed_time}")
 	echo "Execution time: ${elapsed_time} ms" >> "./results/osiris_${filename%.*}.txt"
 done
 
-average_time=$(($total_time / $file_count))
+average_time=$(awk "BEGIN {print $total_time / $file_count}")
+
 echo "Average execution time: ${average_time} ms"
